35ac96ee66054039ec4a604050ad58e42d9a1414
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-03-29 20:20:11 +0530

    writing test and selectedCatagory complete

---
+++ taggingDemo/selectedCategory.swift
@@ -0,0 +1,23 @@
+//
+//  selectedCategory.swift
+//  taggingDemo
+//
+//  Created by Dhrubojyoti on 29/03/20.
+//  Copyright © 2020 Dhrubojyoti. All rights reserved.
+//
+
+import Foundation
+
+enum catagory{
+    case game
+    case animation
+    case story
+    case music
+    case art
+    case experimental
+    case others
+}
+
+class selectedCatagory{
+    var category:catagory!
+}
--- taggingDemoTests/taggingDemoTests.swift
+++ taggingDemoTests/taggingDemoTests.swift
@@ -11,24 +11,30 @@ import XCTest
 
 class taggingDemoTests: XCTestCase {
 
+    var categorySelected:selectedCatagory!
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.categorySelected = selectedCatagory()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testDefaulSelectedCategory() {
+        XCTAssertNil(categorySelected.category)
+    }
+    
+    func testAssignmentSelectedCategory(){
+        self.categorySelected.category = .game
+        XCTAssertEqual(self.categorySelected.category, .game)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSelectedCategoryIsValidCategory() {
+        self.categorySelected.category = .art
+        var isValidCategory:Bool!
+        if self.categorySelected.category == .art || self.categorySelected.category == .game || self.categorySelected.category == .animation || self.categorySelected.category == .music || self.categorySelected.category == .experimental || self.categorySelected.category == .others || self.categorySelected.category == .story{
+            
+            isValidCategory = true
         }
+        XCTAssertTrue(isValidCategory)
     }
 
 }
